c2f7aa63efb421c3f478ff4621718e3fbbd2d332
Abhishek Negi <abhisheknegi@Abhisheks-MacBook-Air.local>
2022-08-03 09:33:14 +0530

    make the environment variables as env based

--- run.py
+++ run.py
@@ -16,10 +16,4 @@ app.register_blueprint(api)
 
 if __name__ == '__main__':
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
-    # When running locally, disable OAuthlib's HTTPs verification.
-    # ACTION ITEM for developers:
-    #     When running in production *do not* leave this option enabled.
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-    os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
     app.run('localhost', 8080, debug=True)
